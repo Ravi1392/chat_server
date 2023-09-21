226663b169219eb0e588617f7dcf33b9366d487c
Ravi <pravis1392@gmail.com>
2023-09-21 18:45:15 +0530

    chat_server

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRouter = require("./src/routes/users.js");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const ConnDatabase = require("./config/database");
+const errorMiddleware = require("./src/middleware/error");
+const cookieParser = require("cookie-parser");
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+//Config
+dotenv.config({ path: "config/config.env" });
+
+//Connecting to Database
+ConnDatabase();
+
+// create express app
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(express.json());
+app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// define a simple route
+app.get("/", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "Welcome to Ecomm app" });
+});
+
+app.use("/", usersRouter);
+
+//Error Handler Middleware
+app.use(errorMiddleware);
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port:${process.env.PORT}`);
+});
+
+//Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
---
+++ src/controllers/Usercontroller.js
@@ -0,0 +1,51 @@
+const User = require("../models/Users");
+const ErrorHander = require("../../utils/errorhander");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const sendToken = require("../../utils/jwtToken");
+
+//Create User
+exports.CreateUser = catchAsyncErrors(async (req, res, next) => {
+  const newUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    profilepic: {
+      public_id: "this is sample id",
+      url: "profilepicUrl",
+    },
+  });
+
+  sendToken(newUser, 201, res);
+});
+
+//Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+  //checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHander("Please Enter Email or Password", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+  sendToken(user, 200, res);
+});
+
+//Logout
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
